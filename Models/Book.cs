ba8c26a6b476fdf0f0785924a38e31b64c0a91ec
mabbasidesign <mabbasidesign.gmil.com>
2021-12-21 14:35:33 +0330

    Add db context

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alabama.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Isbn { get; set; }
+    }
+}
---
+++ Services/AlabamaDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Alabama.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alabama.Services
+{
+    public class AlabamaDBContext: DbContext
+    {
+        public AlabamaDBContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+    }
+}
